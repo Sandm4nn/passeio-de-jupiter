491446c8a46f2c1e30b9538ea0d4debe4b7e72d1
Carlos Henrique <carloshenriqueend@hotmail.com>
2022-04-04 00:44:29 -0300

    adicao das primeiras imagens

--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <h1>Passeio de Jupiter</h1>,
-  document.getElementById('root')
-);
+
+function comic(){
+  const pages = (
+    <div>
+      <img src="src/imgs/street.jpeg" alt="o gato Jupiter olhando para rua" />
+      <img src="src/imgs/boat.jpeg" alt="o gato Jupiter olhando para o barco" />
+      <img src="src/imgs/feather.jpeg" alt="o gato Jupiter com a pena" />
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  )
+  ReactDOM.render(
+    pages,
+    document.getElementById('root')
+  );
+
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
